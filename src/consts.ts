beb395765ba0de91413b732aca3dea34da27c4f6
aber0724 <yanboyuan0724@gmail.com>
2024-09-17 14:40:11 +0800

    update ja translation

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
   TITLE: "aBER",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
+  DESCRIPTION: "Here is aBER's Blog. Sharing my knowledge and experiences. aBERのブログです。私の知識と経験をシェアします。",
   EMAIL: "ab3r77724@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -10,18 +10,18 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "Hi, I'am aBER. こんにちは、aBERです。",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  DESCRIPTION: "A collection of articles, such as study notes and my thoughts. 私の知識と経験をシェアします。",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+    "A collection of my projects with links to repositories and live demos. 私のプロジェクトのリンクを紹介します。",
 };
 
 export const SOCIALS: Socials = [
