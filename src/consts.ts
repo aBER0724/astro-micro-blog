45d74c2ee787bf85743f2a69f08338c6576d76c5
aber0724 <yanboyuan0724@gmail.com>
2024-09-19 16:27:28 +0800

    edit rss descriptions

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@ import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
   TITLE: "aBER",
-  DESCRIPTION: "Here is aBER's Blog. Sharing my knowledge and experiences. \n aBERのブログです。私の知識と経験をシェアします。",
+  DESCRIPTION: "Here is aBER's Blog. Sharing my knowledge and experiences.",
   EMAIL: "ab3r77724@gmail.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
@@ -10,18 +10,18 @@ export const SITE: Site = {
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Hi, I'am aBER. こんにちは、aBERです。",
+  DESCRIPTION: "Hi, I'am aBER.",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles, such as study notes and my thoughts. 私の知識と経験をシェアします。",
+  DESCRIPTION: "A collection of articles, such as study notes and my thoughts.",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos. 私のプロジェクトのリンクを紹介します。",
+    "A collection of my projects with links to repositories and live demos.",
 };
 
 export const SOCIALS: Socials = [
