9c2c8c1a2beb4699b95c51847c51fb72bc777859
aber0724 <yanboyuan0724@gmail.com>
2024-09-23 02:12:45 +0800

    Optimized latex rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.planetaber.site",
   integrations: [tailwind(), sitemap(), mdx(), pagefind()],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+    syntaxHighlight: 'shiki',
     shikiConfig: {
       theme: "css-variables",
     },
